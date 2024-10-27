686b33eb35a644f0e4e114504eab9e0c57f03997
srimant0203 <srimant.acharjya@gmail.com>
2024-10-27 23:50:20 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Logo from '../assets/Logo.png'
+import Code from '../assets/Code.png'
 import { Menu, X } from 'lucide-react'
 
 const Navbar = () => {
@@ -7,17 +7,18 @@ const Navbar = () => {
   return (
     <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4 lg:px-0'>
       <div className=' max-w-7xl mx-auto flex h-14 items-center'>
-        <div className='md:mr-4 flex justify-between w-full'>
-            <a href="#" className='mr-6 flex items-center space-x-2'>
-                <img src={Logo} alt="" className='w-36' />
-            </a>
-            <nav className='md:flex hidden items-center space-x-6 text-lg font-medium'>
-                <a href="#about" className='transition-colors hover:text-foreground/80 text-foreground/60'>About</a>
-                <a href="#projects" className='transition-colors hover:text-foreground/80 text-foreground/60'>Projects</a>
-                <a href="#experience" className='transition-colors hover:text-foreground/80 text-foreground/60'>Experience</a>
-                <a href="#contact" className='transition-colors hover:text-foreground/80 text-foreground/60'>Contact</a>
-            </nav>
-        </div>
+      <div className='md:mr-4 flex justify-between w-full'>
+                    <a href="#" className='mr-6 flex items-center space-x-2'>
+                        {/* Adjust the width here to be smaller */}
+                        <img src={Code} alt="" className='w-20' />
+                    </a>
+                    <nav className='md:flex hidden items-center space-x-6 text-lg font-medium'>
+                        <a href="#about" className='transition-colors hover:text-foreground/80 text-foreground/60'>About</a>
+                        <a href="#projects" className='transition-colors hover:text-foreground/80 text-foreground/60'>Projects</a>
+                        <a href="#experience" className='transition-colors hover:text-foreground/80 text-foreground/60'>Experience</a>
+                        <a href="#contact" className='transition-colors hover:text-foreground/80 text-foreground/60'>Contact</a>
+                    </nav>
+                </div>
         <button className='inline-flex items-center justify-center rounded-md md:hidden' onClick={()=>setmMobileMenuOpen(!mobileMenuOpen)}>
            <span className='sr-only'>Open main menu</span>
            {mobileMenuOpen ? (
